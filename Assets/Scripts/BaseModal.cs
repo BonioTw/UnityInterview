2c7559a2d7bbb3831c1f4c1e631fe06bb6ae81bc
Ansaga <ansagazx@gmail.com>
2017-10-23 18:11:04 +0800

    加入Reference圖

---
+++ Assets/Scripts/BaseModal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class BaseModal : MonoBehaviour {
+  public Text txtTotalAmount;
+  public Button btnIncrease;
+  public Button btnDecrease;
+  protected int totalAmount = 0;
+  protected int totalPrice = 0;
+  protected int unitPrice = 0;
+  protected int maxCount = 0;
+
+  public virtual void Init(){
+    totalAmount = 0;
+    totalPrice = 0;
+    unitPrice = 0;
+    maxCount = 0;
+  }
+
+  public virtual void IncreaseAmount(){
+    totalAmount += 1;
+    if(totalAmount > maxCount)
+      totalAmount = maxCount;
+    totalPrice = txtTotalAmount * unitPrice;
+    txtTotalAmount.text = totalPrice.ToString();
+  }
+
+  public virtual void IncreaseAmount(){
+    totalAmount -= 1;
+    if(totalAmount < 0)
+      totalAmount = 0;
+    totalPrice = txtTotalAmount * unitPrice;
+    txtTotalAmount.text = totalPrice.ToString();
+  }
+
+  public virtual void Close(){
+    //todo
+    //close this modal
+  }
+
+}
